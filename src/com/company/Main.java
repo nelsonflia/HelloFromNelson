34361b385846a26cd14abe6eb068d0b6b1818d74
saraguerreroa <u806792755@byui.edu>
2021-10-25 22:17:27 -0400

    Hello back

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello my name is Nelson");
+        System.out.println("Hello my name is Nelson");
+        System.out.println("Hello Nelson, my name is Sara without 'h'");
     }
 }
